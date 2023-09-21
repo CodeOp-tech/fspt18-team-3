1abea178ab60d39ae1a85f4332034863a3cf8744
Clara-Raposo <clara.ths@gmail.com>
2023-09-21 19:41:55 +0200

    rutas weeks

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render( { title: 'Express' });
 });
 
+/*GET WEEKS */
+router.get('/weeks/:week_number', async function (req, res){
+  try{
+    const result = await db(`SELECT * FROM weeks WHERE id = ${req.params.week_number};`)
+    res.send(result.data)
+  }catch(err){
+    res.status(500).send({ error: err.message })
+  }
+})
+
+router.get('/:week_id/symptoms', async function (req, res){
+  try{
+    const result = await db(`SELECT * FROM weeks_symptoms LEFT JOIN symptoms ON weeks_symptoms.symptom_id = symptoms.id WHERE week_id = ${req.params.week_id};`)
+    res.send(result.data)
+  }catch(err){
+    res.status(500).send({ error: err.message })
+  }
+})
+
+router.get('/:week_id/advice', async function (req, res){
+  try{
+    const result = await db(`SELECT * FROM advice WHERE week_id = ${req.params.week_id};`)
+    res.send(result.data)
+  }catch(err){
+    res.status(500).send({ error: err.message })
+  }
+})
 module.exports = router;
