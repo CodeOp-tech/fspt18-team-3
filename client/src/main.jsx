1001bde39b40f4229a4a6ff1f40fb63c591f273f
Martachis <m.albimuro@gmail.com>
2023-09-24 09:56:53 +0200

    React Router y definición del componente de autenticación

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Login from './routes/Login.jsx'
+import Register from './routes/Register.jsx'
+import User from './routes/User.jsx'
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
+import ErrorPage from "./Error-page.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="login"/>,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/user",
+    element: <User/>,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
